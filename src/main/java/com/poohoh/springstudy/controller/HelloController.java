fc579626143321261cfb3ebb243b3db6015b97bc
poohoh <kjh980328@gmail.com>
2023-02-28 00:51:25 +0900

    feat: hello 컨트롤러

---
+++ src/main/java/com/poohoh/springstudy/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.poohoh.springstudy.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+    @RequestMapping("/hello")
+    public String hello(Model model) {
+        model.addAttribute("data", "hello!");
+        return "hello";
+    }
+}

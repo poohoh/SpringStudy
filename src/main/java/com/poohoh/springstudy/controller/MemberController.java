1fd3f480725068661293b480c2f21841d86f6aff
poohoh <kjh980328@gmail.com>
2023-03-03 19:19:43 +0900

    feat: 회원 웹 기능 - 등록

--- src/main/java/com/poohoh/springstudy/controller/MemberController.java
+++ src/main/java/com/poohoh/springstudy/controller/MemberController.java
@@ -1,9 +1,11 @@
 package com.poohoh.springstudy.controller;
 
-import com.poohoh.springstudy.repository.MemberRepository;
+import com.poohoh.springstudy.domain.Member;
 import com.poohoh.springstudy.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MemberController {
@@ -13,6 +15,22 @@ public class MemberController {
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form) {
+
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
     }
 
 }

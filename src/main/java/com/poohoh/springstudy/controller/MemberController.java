05b00852e61b49313001bf9f48c11bf3698aafd6
poohoh <kjh980328@gmail.com>
2023-03-03 19:24:38 +0900

    feat: 회원 웹 기능 - 조회

--- src/main/java/com/poohoh/springstudy/controller/MemberController.java
+++ src/main/java/com/poohoh/springstudy/controller/MemberController.java
@@ -4,8 +4,12 @@ import com.poohoh.springstudy.domain.Member;
 import com.poohoh.springstudy.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -31,6 +35,14 @@ public class MemberController {
         memberService.join(member);
 
         return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+
+        return "members/memberList";
     }
 
 }
